0a3bb7e92dbdb2a361654ac1f5b4d7b06878877e
enmodoflow <enmodoflow@gmail.com>
2020-03-19 16:06:25 +0100

    primer commit, conexion OK

---
+++ src/main/java/es/plexus/Usuario.java
@@ -0,0 +1,34 @@
+package es.plexus;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Usuario {
+
+	@Id
+    @Column(name = "email")
+    private String email;
+
+	@Column(name = "password")
+    private String pw;
+	
+    @Column(name = "nombre")
+    private String nombre;
+	
+    @Column(name = "apellidos")
+    private String apellidos;
+    
+    @Column(name = "cp")
+    private String cp;
+    
+    @Column(name = "pais")
+    private String pais;
+    
+    
+    public Usuario() {}
+
+}
